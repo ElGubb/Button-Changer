e16c84ec721a8ab87543a2c411924cf15f518d85
ElGubb <gustybaig@hotmail.com>
2020-12-09 13:06:33 -0300

    Revert "Revert "Initial Commit""
    This reverts commit 630c15de2308b1c2e286d3002e43ea19063fcde5.

---
+++ js/actions.js
@@ -0,0 +1,19 @@
+$("#botonred").click(function() {
+    $("#placeholder").css("background-color", "red");
+});
+
+$("#botonyellow").click(function() {
+    $("#placeholder").css("background-color", "yellow");
+});
+
+$("#botongreen").click(function() {
+    $("#placeholder").css("background-color", "green");
+});
+
+$("#botonblue").click(function() {
+    $("#placeholder").css("background-color", "blue");
+});
+
+$("#botonbrown").click(function() {
+    $("#placeholder").css("background-color", "brown");
+});
